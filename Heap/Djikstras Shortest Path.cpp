9fb721a7742f6fda7e0ba11d32194f4a14e8ae05
AdamBJ <anb1@sfu.ca>
2014-11-18 14:10:43 +0900

    final working version

--- Heap/Djikstras Shortest Path.cpp
+++ Heap/Djikstras Shortest Path.cpp
@@ -8,7 +8,7 @@
 
 void ReadFile(std::vector<Edge> adjList[], std::string fileName);
 bool Found_In_VX(int vertexID, const std::vector<int>& X);
-const int NUM_VERTICES = 4;
+const int NUM_VERTICES = 200;
 const int START_VERTEX = 1;
 int main(){
 	//valid entries in adjList start from[1], not zero, so that vertex #1 is stored in [1].
@@ -16,8 +16,8 @@ int main(){
 	std::vector<int> shortestPaths(NUM_VERTICES + 1, 1000000);
 	std::vector<int> X;
 	std::vector<std::string> pathFromStartToVertex(NUM_VERTICES + 1);
-	//ReadFile(adjList, "dijkstraData.txt");
-	ReadFile(adjList, "test.txt");
+	ReadFile(adjList, "dijkstraData.txt");
+	//ReadFile(adjList, "test.txt");
 	
 	X.push_back(START_VERTEX);
 	shortestPaths[START_VERTEX] = 0;
@@ -33,18 +33,20 @@ int main(){
 			//If we find an edge with its head in V-X (V-X = unprocessed vectors. To determine if head is in X-Y, search X for the head.
 			//If not in X, it is in X-Y.
 			for (int q = 0; q < adjList[X[t]].size(); q++){
-
 				//record greedy score for edges that meet our criteria for as we find them
 				int head = adjList[X[t]][q].id;
-				//if head is in VX and the previous cost to head is > the shortest path to X[t] + the edge from X[t] to head
-				if (Found_In_VX(head, X) && shortestPaths[head] > shortestPaths[X[t]] + adjList[X[t]][q].cost){
+				//if head is in VX and the previous cost to head is > the shortest path to X[t] + the edge from X[t] to head, update the path
+				if (Found_In_VX(head, X)){
+					if (shortestPaths[head] > shortestPaths[X[t]] + adjList[X[t]][q].cost){
 						shortestPaths[head] = shortestPaths[X[t]] + adjList[X[t]][q].cost;//our current best guess
-						crossEdges.push_back(adjList[X[t]][q]);
+					}
+					Edge crossEdge(head, shortestPaths[head]);
+					crossEdges.push_back(crossEdge);
 
-						//record the path we used to get to the vertex we just reached (B[vert we've reached] = B[edge tail] + head.
-						std::stringstream ss;
-						ss << pathFromStartToVertex[X[t]] << " " << head;
-						pathFromStartToVertex[head] = ss.str();
+					//record the path we used to get to the vertex we just reached (B[vert we've reached] = B[edge tail] + head.
+					std::stringstream ss;
+					ss << pathFromStartToVertex[X[t]] << " " << head;
+					pathFromStartToVertex[head] = ss.str();
 				}
 			}
 		}
@@ -57,13 +59,13 @@ int main(){
 				}
 			}
 			X.push_back(smallest.id);
-	
 		}
-		
 	}
-	//std::cout << shortestPaths[7] << "," << shortestPaths[37] << "," << shortestPaths[59] << "," << shortestPaths[82];
-	//std::cout << "," << shortestPaths[99] << "," << shortestPaths[115] << "," << shortestPaths[133];
-	//std::cout << "," << shortestPaths[165] << "," << shortestPaths[188] << "," << shortestPaths[197];
+
+	std::ofstream outf("rslts.txt");
+	outf << shortestPaths[7] << "," << shortestPaths[37] << "," << shortestPaths[59] << "," << shortestPaths[82];
+	outf << "," << shortestPaths[99] << "," << shortestPaths[115] << "," << shortestPaths[133];
+	outf << "," << shortestPaths[165] << "," << shortestPaths[188] << "," << shortestPaths[197];
 	return 0;
 }
 
